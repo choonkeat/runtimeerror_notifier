c0eb98bf23be9a9ec16fd5c0de71f7cf65748474
Winston <winston.yongwei@gmail.com>
2014-11-27 16:28:32 +0800

    Wrap in a module so that it works.

--- lib/generators/runtimeerror_notifier/install_generator.rb
+++ lib/generators/runtimeerror_notifier/install_generator.rb
@@ -1,8 +1,10 @@
-class RuntimeerrorNotifier::InstallGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-  desc 'Creates an initializer file for RuntimeError.net at config/initializers'
+module RuntimeerrorNotifier
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+    desc 'Creates an initializer file for RuntimeError.net at config/initializers'
 
-  def install
-    template 'initializer.rb', 'config/initializers/runtimeerror_notifier.rb'
+    def install
+      template 'initializer.rb', 'config/initializers/runtimeerror_notifier.rb'
+    end
   end
 end
